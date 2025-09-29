edeeaf0c5417a02354ffe415ddaa247a9caaa2c3
Henry.Woo <wukeyuan@163.com>
2025-09-29 23:58:45 +0800

    rrelease comments

--- lesson-sql/main.go
+++ lesson-sql/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/henry-woo/golang-study/lesson-sql/curd"
 	"github.com/henry-woo/golang-study/lesson-sql/database"
+	"github.com/henry-woo/golang-study/lesson-sql/sqlx"
 	"github.com/henry-woo/golang-study/lesson-sql/transcation"
 )
 
@@ -46,33 +47,31 @@ func main() {
 	}
 
 	// lesson 3 ~ 4
-	/*
-		{
-			db_sqlx := database.InitSqlxDatabase()
-			sqlx.CreateEmployees(db_sqlx) // create employee test data
+	{
+		db_sqlx := database.InitSqlxDatabase()
+		sqlx.CreateEmployees(db_sqlx) // create employee test data
 
-			// lesson 3.1
-			employees := sqlx.QueryDepartmentEmployees(db_sqlx, "技术部")
-			employees_size := len(employees)
-			if employees_size > 0 {
-				fmt.Println(employees)
-			} else {
-				fmt.Println("No Employees")
-			}
-
-			// lesson 3.2
-			employee := sqlx.QueryMaxSalaryEmployee(db_sqlx)
-			fmt.Println(employee)
-
-			// lesson 4
-			sqlx.CreateBooks(db_sqlx) // create book test data
-			books := sqlx.QueryBestsellingBooks(db_sqlx, 50)
-			book_size := len(books)
-			if book_size > 0 {
-				fmt.Println(books)
-			} else {
-				fmt.Println("No Books")
-			}
+		// lesson 3.1
+		employees := sqlx.QueryDepartmentEmployees(db_sqlx, "技术部")
+		employees_size := len(employees)
+		if employees_size > 0 {
+			fmt.Println(employees)
+		} else {
+			fmt.Println("No Employees")
 		}
-	*/
+
+		// lesson 3.2
+		employee := sqlx.QueryMaxSalaryEmployee(db_sqlx)
+		fmt.Println(employee)
+
+		// lesson 4
+		sqlx.CreateBooks(db_sqlx) // create book test data
+		books := sqlx.QueryBestsellingBooks(db_sqlx, 50)
+		book_size := len(books)
+		if book_size > 0 {
+			fmt.Println(books)
+		} else {
+			fmt.Println("No Books")
+		}
+	}
 }
